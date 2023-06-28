044db3bd735f3706e4927b86ec2f5af1a2a3068a
TheSyllably <goldwynmayerinc@inbox.ru>
2023-06-28 22:25:33 +0400

    12

--- js/script.js
+++ js/script.js
@@ -54,3 +54,55 @@ calcForm.addEventListener('submit', (event) => {
     }
 })
 
+const modalController = ({modal, btnOpen, btnClose, time = 300}) => {
+     const buttonElems = document.querySelectorAll(btnOpen);
+const modalElem = document.querySelector(modal);
+
+modalElem.style.cssText =  `
+display: flex;
+visibility: hidden;
+opacity: 0;
+transition: opacity 300ms ease-in-out;
+`;
+
+const closeModal = event => {
+    const target = event.target;
+
+    if(target === modalElem ||
+        (btnCLose && target.closest(btnClose)) || 
+        event.code === 'Escape') {
+
+        modalElem.style.opacity = 0;
+
+        setTimeout(() => {
+            modalElem.style.visibility = 'hidden';
+        }, 300);
+
+        window.removeEventListener('keydown', closeModal);
+    }
+};
+ 
+const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+};
+
+buttonElems.forEach(btn => {
+     
+btn.addEventListener('click', openModal);
+});
+
+modalElem.addEventListener('click', closeModal);
+};
+
+modalController({
+    modal: '.modal1',
+    btnOpen: '.section__button',
+    btnClose: '.modal__close'
+});
+
+modalController({
+    modal: '.modal2',
+    btnOpen: '.section__button2',
+    btnClose: '.modal__close'
+})
